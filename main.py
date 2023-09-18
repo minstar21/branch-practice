7948006db3bf8eebdc7f1aab07e1f707a99cfcba
minstar21 <min970121@gmail.com>
2023-09-18 11:27:47 +0900

    feat: Wrap functionalize and edit message

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-for i in range(5):
-    print(f'hello for {i+1}th times')
+def print_hello(num:int):
+    """
+    Print hello
+    """
+    for i in range(num):
+        print(f'{i}times hello')
+
+if __name__=='__main__':
+    print_hello(10)
