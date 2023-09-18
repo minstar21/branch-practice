8e72e7eadb38bc56426c33c63a3d27e93fd97bdd
minstar21 <min970121@gmail.com>
2023-09-18 11:51:43 +0900

    commit1

---
+++ main1.py
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+def print_hello(num:int):
+    """
+    Print hello
+    """
+    for i in range(num):
+        print(f'{i}times hello')
+
+        print(f'hello for {num+1}th times')
+    if num % 2 == 0:
+if __name__=='__main__':
+    print_hello(10)
+=======
+for num in range(10):
+>>>>>>> stem
